5e46a7372dcee1d0825f2016d1854e7ff94f5354
Sirija Kasi <sirijagowd00@gmail.com>
2021-04-27 01:00:25 +0530

    node added at particular index

--- ds.js
+++ ds.js
@@ -34,7 +34,33 @@ class LinkedList {
     }
     this.size++;
   }
-  //inserting node in the middle
+  //inserting node at the middle
+  insertAt(data, index) {
+    //index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    //if first index
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    const node = new Node(data);
+    let current, previous;
+    //set current to first
+    current = this.head;
+    let count = 0;
+    while (count < index) {
+      // node before index
+      previous = current;
+      count++;
+      current = current.next; //node after index
+    }
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
 
   //print the List
   printList() {
@@ -51,5 +77,6 @@ list.insertFirst(10);
 list.insertFirst(20);
 list.insertFirst(50);
 list.insertLast(90);
+list.insertAt(80, 2);
 
 list.printList();
