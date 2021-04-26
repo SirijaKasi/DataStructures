a7c0fdf1af211abdf288776088a1d35fbde9dc8f
Sirija Kasi <sirijagowd00@gmail.com>
2021-04-27 00:22:10 +0530

    Last node Inserted

--- ds.js
+++ ds.js
@@ -13,13 +13,43 @@ class LinkedList {
   //inserting first Node
   insertFirst(data) {
     this.head = new Node(data, this.head);
+    this.size++;
   }
   //inserting last node
-  insertLast() {
-      
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+
+    //codition if head is empty or null => head=node
+    if (!this.head) {
+      this.head = node;
+    }
+    //else find lastnode and assign lastnode.next to the head
+    else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+  //inserting node in the middle
+
+  //print the List
+  printList() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
   }
 }
+
 let list = new LinkedList();
 list.insertFirst(10);
 list.insertFirst(20);
-console.log(list);
+list.insertFirst(50);
+list.insertLast(90);
+
+list.printList();
